2ae5118567fb2c090a4721846bd3b167bf9bb77e
Git Demos <tutcollection@gmail.COM>
2018-07-07 18:33:42 +0500

    New footer component # 1

---
+++ footer.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+
+<h1>This is just the test for git merge</h1>
+<?
+echo "a test message from local repo 1";
+echo "a test message from local repo 1";
+?>
+</body>
+</html>
