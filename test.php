4db796977d6a186e1eb5005f8f5ebb87b1471fbe
Git Demos <tutcollection@gmail.COM>
2018-07-07 18:28:51 +0500

    file added

--- test.php
+++ test.php
@@ -7,7 +7,8 @@
 
 <h1>This is just the test for git merge</h1>
 <?
-echo "a test message from local repo 123132";
+echo "a test message from local repo 1";
+echo "a test message from local repo 1";
 ?>
 </body>
 </html>
